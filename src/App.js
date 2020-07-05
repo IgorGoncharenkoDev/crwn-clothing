72b23f1b456a5ce457a0f248e424aa7dd7e7062e
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-07-05 12:09:21 +0300

    Handling the App being aware of auth state change on firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
+
+import { auth } from './firebase/firebase.utils';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/home/home.component';
@@ -8,21 +10,42 @@ import LogInPage from './pages/log-in/log-in.component';
 
 import './styles/styles.scss';
 
-const App = () => (
-  <div className="App">
-    <Header/>
-    <Switch>
-      <Route path="/" exact>
-        <HomePage/>
-      </Route>
-      <Route path="/shop">
-        <ShopPage/>
-      </Route>
-      <Route path="/log-in">
-        <LogInPage/>
-      </Route>
-    </Switch>
-  </div>
-);
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  let unsubscribeFromAuth = null;
+
+  useEffect(() => {
+
+    // subscription
+    unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      // user:
+      // displayName, email
+      setCurrentUser(user);
+    });
+
+    // is triggered on unmount
+    return () => {
+      unsubscribeFromAuth();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <Header/>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage/>
+        </Route>
+        <Route path="/shop">
+          <ShopPage/>
+        </Route>
+        <Route path="/log-in">
+          <LogInPage/>
+        </Route>
+      </Switch>
+    </div>
+  )
+};
 
 export default App;
