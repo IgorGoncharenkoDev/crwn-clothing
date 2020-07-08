080cee3bd14b73329a129e38e98192c85e0770ac
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-07-08 20:36:20 +0300

    PERSISTENCE: persisting cart state to browser local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -6,6 +7,9 @@ import rootReducer from './root-reducer';
 // setting up middlewares
 const middlewares = [ logger ]
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// creating a persisted version of our store
+export const persistor = persistStore(store);
+
+export default { store, persistor };
